f8e73ac38e8fd34549296de60a1b9d3d90d79b2b
Grithi Akshayaa V <141016040+GrithiAkshayaa@users.noreply.github.com>
2025-04-09 10:48:41 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,21 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AddDevicePage from './components/AddDevicePage.jsx'
-import LoginPage from './components/LoginPage.jsx'
-import DeviceDetailsPage from './components/DeviceDetailsPage.jsx'
-import LogoutPage from './components/LogoutPage.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AddDevicePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/device-details" element={<DeviceDetailsPage />} />
-        <Route path="/logout" element={<LogoutPage />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+import AddDevicePage from "./components/AddDevicePage.jsx";
+import LoginPage from "./components/LoginPage.jsx";
+import DeviceDetailsPage from "./components/DeviceDetailsPage.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AddDevicePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/details" element={<DeviceDetailsPage />} />
+    </Routes>
+  </BrowserRouter>
+);
