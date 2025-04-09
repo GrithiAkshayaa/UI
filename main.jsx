2e9fdeffacc72cd80b29d6c89b5e4ea0e7df0cc7
Grithi Akshayaa V <141016040+GrithiAkshayaa@users.noreply.github.com>
2025-04-09 11:24:48 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,17 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
 
-import AddDevicePage from "./components/AddDevicePage.jsx";
-import LoginPage from "./components/LoginPage.jsx";
-import DeviceDetailsPage from "./components/DeviceDetailsPage.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<AddDevicePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/details" element={<DeviceDetailsPage />} />
-    </Routes>
-  </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
