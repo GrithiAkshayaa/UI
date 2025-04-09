83e327e686b203e581bad116bff2123dfcb00004
Grithi Akshayaa V <141016040+GrithiAkshayaa@users.noreply.github.com>
2025-04-09 12:59:05 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import AddDevice from "./pages/AddDevice";
-import DeviceOptions from "./pages/DeviceOptions";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddDevice from "./components/AddDevice";
+import Login from "./components/Login";
+import DeviceOptions from "./components/DeviceOptions";
+import ConfigurationManagement from "./components/ConfigurationManagement";
+import PerformanceManagement from "./components/PerformanceManagement";
+import FaultManagement from "./components/FaultManagement";
+import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AddDevice />} />
-      <Route path="/device-options/:deviceName" element={<DeviceOptions />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AddDevice />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/device-options/:deviceType" element={<DeviceOptions />} />
+        <Route path="/configuration" element={<ConfigurationManagement />} />
+        <Route path="/performance" element={<PerformanceManagement />} />
+        <Route path="/fault" element={<FaultManagement />} />
+      </Routes>
+    </Router>
   );
 }
 
