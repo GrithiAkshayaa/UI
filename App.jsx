09abf6f4046411cc6ae3beaa4ee8aa965fefd99b
Grithi Akshayaa V <141016040+GrithiAkshayaa@users.noreply.github.com>
2025-04-09 11:52:01 +0530

    Update App.jsxl

--- App.jsx
+++ App.jsx
@@ -1,16 +1,12 @@
-// src/App.jsx
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AddDevice from "./pages/AddDevice";
-import LoginPage from "./pages/LoginPage";
 import DeviceOptions from "./pages/DeviceOptions";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AddDevice />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/device-options" element={<DeviceOptions />} />
+      <Route path="/device-options/:deviceName" element={<DeviceOptions />} />
     </Routes>
   );
 }
